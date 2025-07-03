2891f3b48247b0cdf35a270b17aa7fabb3e9fe1d
PiyushGarg0205 <piyushgarg0205@gmail.com>
2025-07-03 12:34:46 +0530

    First Commit

---
+++ 1.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "My new journey!" << std::endl;
+    return 0;
+}
---
+++ 2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+int main() {
+    int number = 40;
+    double decimal = 3.14;
+    char character = 'A';
+    std::cout << "Integer: " << number << std::endl;
+    std::cout << "Decimal: " << decimal << std::endl;
+    std::cout << "Character: " << character << std::endl;
+    return 0;
+}
